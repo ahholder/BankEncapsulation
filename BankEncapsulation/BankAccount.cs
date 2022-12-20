a341d4bca427e04de720be3a968372a2086f1781
Alexander Holder <drowish95z@gmail.com>
2022-12-20 14:01:29 -0600

    assignment + flavor

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+        public void Deposit(double cash)
+        {
+            balance += cash;
+        }
+        public void GetBalance()
+        {
+            Console.Write(balance);
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            string input = "";
+            double placeholder;
+            double transaction = 0;
+            var account = new BankAccount();
+            Console.Write("Deposited Amount: ");
+            input = Console.ReadLine();
+            if (input != null && double.TryParse(input, out placeholder) == true)
+            {
+                transaction = double.Parse(input);
+            }
+            Console.WriteLine("You added $" + transaction + " to your account.");
+            account.Deposit(transaction);
+            //double balanced = account.GetBalance();
+            //Console.WriteLine("");
+            Console.Write("Your balance is now $");
+            account.GetBalance();
+            Console.Write(".");
+            Console.WriteLine("");
         }
     }
 }
